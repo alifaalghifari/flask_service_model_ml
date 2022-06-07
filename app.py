91c862ead23c933383d0d1d47e2b568acc690f6e
Muh alif alghifari <alifalghifari798@gmail.com>
2022-06-07 22:14:22 +0800

    add GET recommendation based on name of pesticede

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import (Flask,  render_template, request
                    )
 from keras.models import load_model
-from keras.preprocessing import image
 import tensorflow as tf
 import numpy as np
 import os
@@ -25,33 +24,59 @@ df = pickle.load(open("model_ml/recommend_data.pkl", "rb"))
 similarity = pickle.load(open("model_ml/similarity.pkl", "rb"))
 # list_movie = np.array(df["cleaned_desc"])
 
-
-def recommend_pestisida():
-
-    # nama = request.form['nama']
-    # jenis = request.form['jenis']
-    # data = df.loc[df['kegunaan'] == jenis]
-    # data.reset_index(level=0, inplace=True)
-
+@app.route('/recommend/<pesticide_name>')
+def recommend_pestisida_name(pesticide_name):
     indices = pd.Series(df.index, index=df['nama'])
 
+    idx = indices[pesticide_name]
+
+    sig = list(enumerate(similarity[idx]))  # Sort the names
+
+    # Scores of the 5 most similar name
+    sig = sorted(sig, key=lambda x: x[1], reverse=True)
+
+    sig = sig[1:10]  # Book indicies
+    pesticide_indices = [i[0] for i in sig]
+
+    #   # Top 5 pesticide recommendation
+    rec = df[['nama', 'kegunaan', 'tempat',
+              'berat', 'image-src', 'product_link']].iloc[pesticide_indices]
+
+    # response_json = {
+        #     recommendations : rec
+        # }
+    # return jsonify(response_json)
+
+    return render_template('resultRecommend.html', recommend = rec)
+
+def recommend_pestisida(disease):
+    indices = pd.Series(df.index, index=df['nama'])
+
+    if disease == 'Leaf Blast':
+        pesticide_name = 'Filia 525Se 250Ml Obat Hawar Daun Dan Blast Original'
+    elif disease == 'Brown Spot':
+        pesticide_name = 'Fungisida REMAZOLE - P 490 EC - 250 ml'
+    elif disease == 'Hispa':
+        pesticide_name = 'nararel 550 EC 400 ML'
+    else:
+        return ''
     # Get the pairwsie similarity scores
-    idx = indices['Filia 525Se 250Ml Obat Hawar Daun Dan Blast Original']
+    idx = indices[pesticide_name]
     # return idx
     # return idx
     # print(idx)
     sig = list(enumerate(similarity[idx]))  # Sort the names
     # return sig
-    # Scores of the 5 most similar books
+    # Scores of the 5 most similar name
     sig = sorted(sig, key=lambda x: x[1], reverse=True)
     # return sig
 
     sig = sig[1:10]  # Book indicies
-    tourist_indices = [i[0] for i in sig]
+    pesticide_indices = [i[0] for i in sig]
 
-    #   # Top 5 tourist recommendation
+    #   # Top 5 pesticide recommendation
     rec = df[['nama', 'kegunaan', 'tempat',
-              'berat', 'image-src', 'product_link']].iloc[tourist_indices]
+              'berat', 'image-src', 'product_link']].iloc[pesticide_indices]
 
     # print(rec)
     return rec
@@ -82,26 +107,27 @@ def result_model():
 
         # ['LeafBlast', 'Healthy', 'BrownSpot', 'Hispa']
         if result == 0:
-            name = 'LeafBlast'
+            disease = 'LeafBlast'
         elif result == 1:
-            name = 'Healthy'
+            disease = 'Healthy'
         elif result == 2:
-            name = 'BrownSpot'
+            disease = 'BrownSpot'
         else:
-            name = 'Hispa' 
+            disease = 'Hispa' 
             
         # get pesticide recommend
-        rec = recommend_pestisida()
+        # disease = disease
+        rec = recommend_pestisida(disease = 'Brown Spot')
 
         # return json 
         # response_json = {
-        #     name : name,
+        #     disease : disease,
         #     recommendations : rec
         # }
         # return jsonify(response_json)
 
        
         # return web
-        return render_template('resultModel.html', training=str(classes), hasil=str(result), nama=name,recommend=rec )
+        return render_template('resultModel.html', training=str(classes), hasil=str(result), nama=disease,recommend=rec )
 if __name__ == '__main__':
     app.run(debug=True)
